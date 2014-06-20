5e1fb105e35aded18124253901634d113ebc8446
Sanjeev Sharma <sksharma1501@gmail.com>
2014-06-20 13:46:21 -0400

    More updates

--- README.md
+++ README.md
@@ -10,9 +10,10 @@ ii) Reads Train data files into dataframe. It converts activity codes to descrip
 iii) Reads Test data files into dataframe. It converts activity codes to descriptive names  and labels the coulmn names. <br>
 iv) Merges the above two dataframes with cbind(). <br>
 v) Now reduces the above joint dataframe, by retaining only the mean and standard deviation columns. <br>
-vi) Splits the reduced dataframe based on the "subject". <br>
-vii) Splits the above frame further based on the "activity". <br>
-viii) Applies helper function to find columns means for all sub-lists (multiple lapply). Note that the helper function removes the factor columns viz. "subject" and "activity" before applying colMeans. <br>
-ix) Writes the data into an intermediate csv file. <br> 
-x) Reads back the csv file and transforms columns to rows in the new dataframe. Writes the new tidy data into another .csv file, which is a text file and easily readable in spreadsheet software. <br><br>
+vi) Tidies up the variable names by removing braces, changing "-" to "." and changing to lower-case.
+vii) Splits the reduced dataframe based on the "subject". <br>
+viii) Splits the above frame further based on the "activity". <br>
+ix) Applies helper function to find columns means for all sub-lists (multiple lapply). Note that the helper function removes the factor columns viz. "subject" and "activity" before applying colMeans. <br>
+x) Writes the data into an intermediate csv file. <br> 
+xi) Reads back the csv file and transforms columns to rows in the new dataframe. Writes the new tidy data into another .csv file, which is a text file and easily readable in spreadsheet software. <br><br>
 <b> Note: </b> For Coursera uploading purpose, .txt extension has been added to the output .csv file. Though it can be opened in any text editor, it would be easier to read in spreadsheet software like Excel (first remove the .txt extension). <br>
--- run_analysis.R
+++ run_analysis.R
@@ -16,35 +16,41 @@ act_df <- read.table("activity_labels.txt")
 #head(train_df)
 #nrow(train_df)
 train_df[,2] <- read.table("./train/y_train.txt")
-train_df[,2] <- act_df[train_df[,2],2]
+train_df[,2] <- tolower(act_df[train_df[,2],2])
 train_df[,3:563] <- read.table("./train/X_train.txt")
 #head(train_df,1)
 feat_df1 <- read.table("features.txt")
 feat_vector <- as.vector(feat_df1$V2)
 names(train_df) <- c("subject","activity",feat_vector)
-head(train_df[,1:5])
+#head(train_df[,1:5])
 
 # Read Test data and label it
 test_df <- read.table("./test/subject_test.txt")
 test_df[,2] <- read.table("./test/y_test.txt")
-test_df[,2] <- act_df[test_df[,2],2]
+test_df[,2] <- tolower(act_df[test_df[,2],2])
 test_df[,3:563] <- read.table("./test/X_test.txt")
 names(test_df) <- c("subject","activity",feat_vector)
-head(test_df[,1:5])
+#head(test_df[,1:5])
 
 # Merge data
 train_test_df <- rbind(test_df, train_df)
-dim(train_test_df)
+#dim(train_test_df)
 #head(train_test_df)
 
 # Extract mean and standard deviation measurements only
-names(train_test_df)
-extractCols <- grep("mean[(]|std[(]", names(train_test_df)) # %in% c("tBodyAcc-mean()-X", "std") 
+#names(train_test_df) 
+extractCols <- grep("mean[(]|std[(]", names(train_test_df))  
 #extractCols
 train_test_df.reduce <- cbind(train_test_df[,1:2], train_test_df[, extractCols])
-names(train_test_df.reduce)
+
+# Tidy up variable names
+names(train_test_df.reduce)  <- gsub("\\(","",names(train_test_df.reduce))
+names(train_test_df.reduce)  <- gsub("\\)","",names(train_test_df.reduce))
+names(train_test_df.reduce)  <- gsub("-",".",names(train_test_df.reduce))
+names(train_test_df.reduce)  <- tolower(names(train_test_df.reduce))
+#names(train_test_df.reduce)
 #ndatcol <- ncol(train_test_df.reduce) - 2 # How many data columns?
-head(train_test_df.reduce[,1:6])
+#head(train_test_df.reduce[,1:6])
 
 
 # Split based on subject 
@@ -60,6 +66,26 @@ train_test_df.subj.act <- lapply(train_test_df.subj, function(x) {split(x, x$act
 train_test_df.subj.act.mean <- lapply(train_test_df.subj.act, function(x) {lapply(x, meanList)})
 #str(train_test_df.subj.act.mean)
 
-# Write tidy data into output file
+# Write tidy data into intermediate output file
 write.csv(train_test_df.subj.act.mean, "output.csv")
+
+# Transform from rows to columns
+csv_df <- read.csv("output.csv")
+colnames <- as.vector(csv_df[,1])
+rownames <- as.vector(names(csv_df))
+rownames <- rownames[-1]
+csv_df <- csv_df[,-1]
+#csv_df[1:4,1:4]
+rows <- nrow(csv_df)
+cols <- ncol(csv_df)
+new_csv_df <- matrix(nrow=cols,ncol=rows)
+colnames(new_csv_df) <- colnames
+rownames(new_csv_df) <- rownames
+for (i in 1:cols)
+{
+  new_csv_df[i,] <- csv_df[,i]
+}
+
+#new_csv_df[1:4,1:4]
+write.csv(new_csv_df,"tidyoutput.csv")
 
